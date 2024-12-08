d5d53577615943577042676eff0e84351c2f89e7
Csanad Kolcsei <kcsanad98@gmail.com>
2024-12-08 13:20:50 +0100

    Initial repo setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new LoggingInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('Advanced Stock Price Checker API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
+  const host = process.env['API_HOST'];
+  const port = process.env['API_PORT'];
+
+  await app.listen(port, host);
 }
 bootstrap();
+
+const defaultErrorHandler = (error: unknown) => {
+  Logger.error(error, 'Process');
+};
+
+process.on('unhandledRejection', defaultErrorHandler);
+
+process.on('uncaughtException', defaultErrorHandler);
